46dd12d6f66af92f4fe97da2a223390d469a19ae
james.lau@gta-travel.com <jameslauwf@gmail.com>
2019-01-28 11:46:33 +0000

    Fix win CTRL issues in startBootApp.sh -- set ff=unix

--- startBootApp.sh
+++ startBootApp.sh
@@ -2,7 +2,7 @@ snapshotJar=demo-0.0.1-SNAPSHOT.jar
 
 echo " SpringBoot application ${snapshotJar}......"
 
-JAVA_OPTS="-Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=8000,suspend=n "
+JAVA_OPTS="-Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=8005,suspend=n "
 
 JAVA_OPTS=" ${JAVA_OPTS} -XX:+UseG1GC"
 JAVA_OPTS=" ${JAVA_OPTS} -verbose:gc  -XX:+PrintGCDateStamps -XX:+PrintGCDetails -Xloggc:./logs/gc-demo.log"
